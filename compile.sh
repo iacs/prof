96cdfee5154042b88f8914fe335dd8d653e71784
Iago Mosquera <flackon@gmail.com>
2022-11-03 16:39:15 +0100

    Updated script

--- compile.sh
+++ compile.sh
@@ -5,4 +5,3 @@ SRCURL="http://github.com/iacs"
 source venv/bin/activate
 qr --factory=svg-path $SRCURL > assets/img/qrcode.svg
 python3 stamper.py -i cvdata.json cvtemplate.jinja > cv.html
-weasyprint cv.html cv.pdf
